682dfe87feb0d365dcd66dd716d1e4211850a3c3
BeggiNerd00 <bjartmar.hallgrimsson@gmail.com>
2021-10-15 13:39:53 +0000

    completed

--- Assignment_4/answer_2.dart
+++ Assignment_4/answer_2.dart
@@ -4,6 +4,10 @@
 /// b.  and smaller or equal to 80.
 ///2. Finaly print how many number met those conditions.
 
+
+
+
+
 void main() {
   List<int> calculate_sum = [13, 56, 77, 3, 45, 6, 944, 1, 43, 7, 40, 55, 67, 93, 45, 754, 33];
   int counter = 0;
